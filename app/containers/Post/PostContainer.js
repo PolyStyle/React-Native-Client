e25c0a716ab0904d66418b0b12f8b3c4e8e56d6f
Nicola Bortignon <info@nicolabortignon.com>
2017-02-28 21:03:43 +0100

    fixing post container issue with new post representation

--- app/containers/Post/PostContainer.js
+++ app/containers/Post/PostContainer.js
@@ -2,7 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { ScrollView, View, Text, StyleSheet, Image, ListView, Dimensions, TouchableOpacity, InteractionManager} from 'react-native';
 import { Gear, Hamburger, Heart, TagLabel, MoreDots, ScaledImage} from './../../components';
-import { fetchPost } from './../../redux/modules/posts';
+import {  hasLikedPost,unlikePost , likePost } from './../../redux/modules/posts';
 
 const { height,width } = Dimensions.get('window');
 
@@ -123,74 +123,42 @@ class PostContainer extends Component {
     super(props)
     this.state = {
         active: false,
+        currentIndexInPosts: -1,
       };
   }
 
   componentDidMount() {
-    this.props.dispatch(fetchPost(this.props.id));
+    let currentPost = null;
+    let index = -1;
+    for(var i = 0; i < this.props.posts.length; i++){
+      if(this.props.posts[i].id == this.props.id){
+        index = i;
+      }
+    }
+
+    this.setState({
+      currentIndexInPosts : index,
+    })
+    //this.props.dispatch(fetchPost(this.props.id));
   }
 
 
   componentDidUpdate(prevProps, prevState){
-    // I've received new props, I need to decide if I need to update the data store or not.
-    // (prevProps, prevState)
-    /*
-     console.log('--- COMPONENT DID UPDATE --- ')
-     try{
-     console.log('this.props.post.id' , this.props.post.id)
-     console.log('this.state.dataSource' ,this.state.dataSource)
-     console.log('prevstate.id != post.id', this.props.id != this.props.post.id)
-     console.log('FIRST CONDITION ', (this.props.post.id && this.prevState == undefined))
-     console.log('Second condition ',  (this.props.post.id != this.prevState.id))
-     } catch(e){}
-     */
-    // update only if you have a post Id and if you didn't have generate already a datasource.
 
-    /*. TODO NICOLA: temporary removed from our
-    if((this.props.post.id && this.state.dataSource == undefined) || (this.props.post.id != this.props.id)){
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      // this.props.post.Products.PostProduct.category
-
-      var dictionary = [];
-      for(var i = 0; i<this.props.post.Products.length; i++){
-        if(!dictionary[this.props.post.Products[i].PostProduct.category]){
-          dictionary[this.props.post.Products[i].PostProduct.category] = [];
-        }
-        dictionary[this.props.post.Products[i].PostProduct.category].push(this.props.post.Products[i]);
-      }
-
-      var itemDataSources = [];
-      var keys = [];
-      for (var key in dictionary) {
-        keys.push(key);
-        itemDataSources.push(ds.cloneWithRows(dictionary[key]));
-      }
-
-      var allProducts = [];
-      for(var i = 0; i<this.props.post.Products.length; i++){
-        allProducts.push(this.props.post.Products[i]);
-      }
-
-      this.setState({
-        keys: keys,
-        dataSource: itemDataSources,
-        allProducts: ds.cloneWithRows(allProducts),
-      });
-
-    } */
   }
 
 
-  onPress = () =>{
-    const newState = !this.state.active;
-    this.setState({
-          active: newState
-        }, function(){
-    if(this.props.onPress) {
-      this.props.onPress()
+  likePost(){
+    if(this.props.posts[this.state.currentIndexInPosts].isLiking){
+      // remove the follow
+      this.props.dispatch(unlikePost(this.props.id));
+    } else {
+      // add the follow
+      this.props.dispatch(likePost(this.props.id));
     }
-    });
   }
+
+
 
   _navigateToBrand(brandData){
   this.props.navigator.push({
@@ -219,11 +187,16 @@ class PostContainer extends Component {
 
 
   render(){
-    const currentPost = this.props.posts[this.props.id];
-
+    let currentPost = null;
+    if(this.state.currentIndexInPosts > -1){
+      currentPost = this.props.posts[this.state.currentIndexInPosts];
+    }
+    if(!currentPost){
+      return (<View />)
+    }
     return (
       <ScrollView style={styles.container}>
-       <TouchableOpacity onPress={this.onPress.bind(this)}>
+       <TouchableOpacity onPress={()=>{}}>
           <ScaledImage
             id={currentPost.ImageId}
             width={width}
@@ -231,8 +204,8 @@ class PostContainer extends Component {
        </TouchableOpacity>
        <View style={styles.descriptions}>
         <View style={styles.iconContainer}>
-          <Heart active={this.state.active} style={styles.heartIcon} onPress={this.onPress.bind(this)}/>
-          <MoreDots style={styles.addIcon} onPress={this.onPress.bind(this)}/>
+          <Heart active={currentPost.isLiking} style={styles.heartIcon} onPress={this.likePost.bind(this)}/>
+          <MoreDots style={styles.addIcon} />
         </View>
         <View style={styles.separationLine} />
         <Text style={styles.descriptionText}>Posted by: {currentPost.User.displayName} </Text>
